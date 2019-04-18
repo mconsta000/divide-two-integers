87577710986782770062adf5dfccbb2dbeb8480a
mcosnta000 <mconsta000@gmail.com>
2019-04-18 19:06:05 -0400

    Handle negative integers

--- divide-two-numbers/src/main/java/org/mconsta000/divide/App.java
+++ divide-two-numbers/src/main/java/org/mconsta000/divide/App.java
@@ -4,11 +4,24 @@ package org.mconsta000.divide;
 public final class App {
     public int divide(int dividend, int divisor) {
 
+        int dividendSign = 1;
+        int divisorSign = 1;
+
+        if (dividend < 0){
+            dividendSign = -1;
+            dividend *= dividendSign;
+        }
+
+        if (divisor < 0){
+            divisorSign = -1;
+            divisor *= divisorSign;
+        }
+
         int ret = 0;
         while (dividend >= divisor){
             ret++;
             dividend -= divisor;
         }
-        return ret;        
+        return ret * dividendSign * divisorSign;        
     }
 }
--- divide-two-numbers/src/test/java/org/mconsta000/divide/AppTest.java
+++ divide-two-numbers/src/test/java/org/mconsta000/divide/AppTest.java
@@ -8,7 +8,32 @@ public class AppTest {
     private App app = new App();
 
     @Test
-    public void testApp() {
+    public void testPositive() {
         assertEquals(5, app.divide(10,2));
+    }
+
+    @Test
+    public void testNegativeDivisor() {
+        assertEquals(-5, app.divide(10,-2));
+    }
+
+    @Test
+    public void testNegativeDividend() {
+        assertEquals(-5, app.divide(-10,2));
+    }
+
+    @Test
+    public void testNegativeDividendAndDivisor() {
+        assertEquals(5, app.divide(-10,-2));
+    }
+
+    @Test
+    public void testRemainder() {
+        assertEquals(5, app.divide(11,2));
+    }
+
+    @Test
+    public void testZeroDividend() {
+        assertEquals(0, app.divide(0,2));
     }
 }
