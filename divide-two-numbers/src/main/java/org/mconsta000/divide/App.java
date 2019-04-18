25198b726a61f8fc854115b49c0064f2acba6125
mcosnta000 <mconsta000@gmail.com>
2019-04-18 19:58:53 -0400

    Playing the two'

--- divide-two-numbers/src/main/java/org/mconsta000/divide/App.java
+++ divide-two-numbers/src/main/java/org/mconsta000/divide/App.java
@@ -4,24 +4,28 @@ package org.mconsta000.divide;
 public final class App {
     public int divide(int dividend, int divisor) {
 
-        int dividendSign = 1;
-        int divisorSign = 1;
+        boolean negate = false;
 
-        if (dividend < 0){
-            dividendSign = -1;
-            dividend *= dividendSign;
+        if ((dividend & Integer.MIN_VALUE) == Integer.MIN_VALUE) {
+            negate = !negate;
+            dividend = (dividend ^ -1) + 1;
         }
 
-        if (divisor < 0){
-            divisorSign = -1;
-            divisor *= divisorSign;
+        if ((divisor & Integer.MIN_VALUE) == Integer.MIN_VALUE) {
+            negate = !negate;
+            divisor = (divisor ^ -1) + 1;
         }
 
         int ret = 0;
-        while (dividend >= divisor){
+        while (dividend >= divisor) {
             ret++;
             dividend -= divisor;
         }
-        return ret * dividendSign * divisorSign;        
+
+        if (negate) {
+            ret = (ret-1) ^ -1;
+        }
+
+        return ret;        
     }
 }
