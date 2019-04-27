5b7ab1572d453f5d4ed72cee832417df0b9a36c6
mcosnta000 <mconsta000@gmail.com>
2019-04-27 12:10:46 -0400

    Postive Integer Apporach

--- divide-two-numbers/src/main/java/org/mconsta000/divide/App.java
+++ divide-two-numbers/src/main/java/org/mconsta000/divide/App.java
@@ -2,25 +2,40 @@ package org.mconsta000.divide;
 
 
 public final class App {
+    private final static int SIGNED_WIDTH = 31;
+    private final static int MASK = 0x60000000;
+
+
+    protected int buildMask(int size) {
+        int bit_mask = 0x40000000;
+        int mask = 0;
+
+        for (int i=0; i<size; i++) {
+            mask = mask | bit_mask;
+            bit_mask = bit_mask >> 1;
+        }
+        return mask;
+    }
+
+    protected int extract(int dividend, int mask, int mask_size, int bit_pos) {
+        int work = dividend & mask;
+        return (work >> (bit_pos + 1 - mask_size));
+    }
+
     public int divide(int dividend, int divisor) {
+        int mask_size = Integer.highestOneBit(divisor);
+        int mask = buildMask(mask_size);
+        int remainder = 0;
         int answer = 0;
-        int template = 1;
-        int base = Integer.bitCount(iDivisor);
-        int leftOver = 0;
 
-        for (int i=31; i>=base; i--) {
-            int mask = template << i;
-            int current = (iDividend & mask) + leftOver;
+        for (int i=30; i>=(mask_size-1); i--) {
+            int work = extract(dividend, mask, mask_size, i) + remainder;
 
-            answer = answer << 1;
-            if (iDivisor <= current) {
-                answer = answer | 1;
-                template = 1;
-                leftOver = current - iDivisor;
-            }
-            else {
-                template = template << 1;
-                template = template | 1;
+            answer = (answer << 1); 
+            mask = mask >> 1;
+            if (work >= divisor) {
+                remainder = work - divisor;
+                answer = answer | 0x00000001;
             }
         }
 
--- divide-two-numbers/src/test/java/org/mconsta000/divide/AppTest.java
+++ divide-two-numbers/src/test/java/org/mconsta000/divide/AppTest.java
@@ -7,7 +7,52 @@ import static org.junit.Assert.*;
 public class AppTest {
     private App app = new App();
 
-    @Test
+    @Test(timeout = 500)
+    public void testMask1() {
+        // 0100 0000 0000 0000 0000 0000 0000 0000
+        //    4    0    0    0    0    0    0    0 
+        assertEquals(Integer.toBinaryString(0x40000000), Integer.toBinaryString(app.buildMask(1)));
+    }
+
+    @Test(timeout = 500)
+    public void testMask2() {
+        // 0110 0000 0000 0000 0000 0000 0000 0000
+        //    6    0    0    0    0    0    0    0 
+        assertEquals(Integer.toBinaryString(0x60000000), Integer.toBinaryString(app.buildMask(2)));
+    }
+
+    @Test(timeout = 500)
+    public void testMask4() {
+        // 0111 1000 0000 0000 0000 0000 0000 0000
+        //    7    8    0    0    0    0    0    0 
+        assertEquals(Integer.toBinaryString(0x78000000), Integer.toBinaryString(app.buildMask(4)));
+    }
+
+    @Test()
+    public void testExtract1() {
+        // 0100 0000 0000 0000 0000 0000 0000 0000
+        //    4    0    0    0    0    0    0    0 
+        assertEquals(Integer.toBinaryString(0x00000001), 
+            Integer.toBinaryString(app.extract(0x70000000, 0x40000000, 1, 30)));
+    }
+
+    @Test()
+    public void testExtract7() {
+        // 0100 0000 0000 0000 0000 0000 0000 0000
+        //    4    0    0    0    0    0    0    0 
+        assertEquals(Integer.toBinaryString(0x00000007), 
+            Integer.toBinaryString(app.extract(0x70000000, 0x70000000, 2, 29)));
+    }
+
+    @Test()
+    public void testExtract10() {
+        // 0000 0000 0000 0000 0000 0000 0000 1010
+        //    0    0    0    0    0    0    0    a 
+        assertEquals(Integer.toBinaryString(0x00000002), 
+            Integer.toBinaryString(app.extract(0x0000000a, 0x0000000c, 2, 3)));
+    }
+
+    @Test()
     public void testPositive() {
         assertEquals(5, app.divide(10,2));
     }
@@ -17,42 +62,42 @@ public class AppTest {
         assertEquals(-5, app.divide(10,-2));
     }
 
-    @Test
+    @Test(timeout = 500)
     public void testNegativeDividend() {
         assertEquals(-5, app.divide(-10,2));
     }
 
-    @Test
+    @Test(timeout = 500)
     public void testNegativeDividendAndDivisor() {
         assertEquals(5, app.divide(-10,-2));
     }
 
-    @Test
+    @Test(timeout = 500)
     public void testRemainder() {
         assertEquals(5, app.divide(11,2));
     }
 
-    @Test
+    @Test(timeout = 500)
     public void testZeroDividend() {
         assertEquals(0, app.divide(0,2));
     }
 
-    @Test
+    @Test(timeout = 500)
     public void testOverflow() {
         assertEquals(Integer.MAX_VALUE, app.divide(Integer.MIN_VALUE,-1));
     }
 
-    @Test
+    @Test(timeout = 500)
     public void testOverflow2() {
         assertEquals(Integer.MIN_VALUE, app.divide(Integer.MIN_VALUE,1));
     }
 
-    @Test
+    @Test(timeout = 500)
     public void testUnderflow() {
         assertEquals(-Integer.MAX_VALUE, app.divide(Integer.MAX_VALUE,-1));
     }
 
-    @Test
+    @Test(timeout = 500)
     public void testUnderflow2() {
         assertEquals(Integer.MAX_VALUE, app.divide(Integer.MAX_VALUE,1));
     }
