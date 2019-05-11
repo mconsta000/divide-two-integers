f2f20495edea253b525d7272712692ff1dfd304b
mcosnta000 <mconsta000@gmail.com>
2019-05-11 14:50:26 -0400

    Parameterize JUNIT test

--- divide-two-numbers/src/test/java/org/mconsta000/divide/AppTest.java
+++ divide-two-numbers/src/test/java/org/mconsta000/divide/AppTest.java
@@ -1,74 +1,52 @@
 package org.mconsta000.divide;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class AppTest {
     private App app = new App();
 
-    @Test(timeout = 500)
-    public void testPositive() {
-        assertEquals(5, app.divide(10,2));
+    @Parameter(0)
+    public int dividend;
+
+    @Parameter(1)
+    public int divisor;
+
+    @Parameter(2)
+    public int expected;
+
+    @Parameters(name="{index}: {0}/{1}={2}")
+    public static Collection<Object[]> data() {
+        Object data[][] = new Object[][] {
+            {10,2,5},
+            {10,-2,-5},
+            {-10,2,-5},
+            {-10,-2,5},
+            {11,2,5},
+            {10,3,3},
+            {30,4,7},
+            {101,4,25},
+            {0,2,0},
+            {Integer.MIN_VALUE,-1,Integer.MAX_VALUE},
+            {Integer.MIN_VALUE,1,Integer.MIN_VALUE},
+            {Integer.MAX_VALUE,-1,-Integer.MAX_VALUE},
+            {Integer.MAX_VALUE,1,Integer.MAX_VALUE} 
+        };
+
+        return Arrays.asList(data);
     }
 
-    @Test(timeout = 500)
-    public void testNegativeDivisor() {
-        assertEquals(-5, app.divide(10,-2));
-    }
-
-    @Test(timeout = 500)
-    public void testNegativeDividend() {
-        assertEquals(-5, app.divide(-10,2));
-    }
-
-    @Test(timeout = 500)
-    public void testNegativeDividendAndDivisor() {
-        assertEquals(5, app.divide(-10,-2));
-    }
-
-    @Test(timeout = 500)
-    public void testRemainder() {
-        assertEquals(5, app.divide(11,2));
-    }
-
-    @Test(timeout = 500)
-    public void testOddDivisor() {
-        assertEquals(3, app.divide(10,3));
-    }
-
-    @Test(timeout = 500)
-    public void testRemainder2() {
-        assertEquals(7, app.divide(30,4));
-    }
-
-    @Test(timeout = 500)
-    public void testRemainder3() {
-        assertEquals(25, app.divide(101,4));
-    }
-
-    @Test(timeout = 500)
-    public void testZeroDividend() {
-        assertEquals(0, app.divide(0,2));
-    }
-
-    @Test(timeout = 500)
-    public void testBounary1() {
-        assertEquals(Integer.MAX_VALUE, app.divide(Integer.MIN_VALUE,-1));
-    }
-
-    @Test()
-    public void testBounary2() {
-        assertEquals(Integer.MIN_VALUE, app.divide(Integer.MIN_VALUE,1));
-    }
-
-    @Test(timeout = 500)
-    public void testBoundary3() {
-        assertEquals(-Integer.MAX_VALUE, app.divide(Integer.MAX_VALUE,-1));
-    }
-
-    @Test(timeout = 500)
-    public void testBoundary4() {
-        assertEquals(Integer.MAX_VALUE, app.divide(Integer.MAX_VALUE,1));
+    @Test
+    public void test() {
+        assertEquals(expected, app.divide(dividend, divisor));
     }
 }
